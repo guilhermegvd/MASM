a1f879fe9b9c59f23eab2a261696d1233b123b47
Guilherme Vieira Dutra <guilhermegvd@users.noreply.github.com>
2023-02-24 01:10:42 -0300

    Finished development

--- wallpaper/README.md
+++ wallpaper/README.md
@@ -1,15 +1,10 @@
-# Glass
+# Bing Wallpaper
 
-- Este programa construído em Assembly x86 abre uma janela transparente com efeito Aero Glass no Windows Vista / 7.
-
-![Efeito vidro do Aero Glass na janela inteira](https://guilhermevieiradutra.com.br/github/b546fdhr84.png)
-
-### Processo
-- Para atingir este resultado devemos definir a cor de fundo da janela para preto (hbrBackground = 0x0h) e chamamos a função **DwmExtendFrameIntoClientArea** (dwmapi.lib) após **UpdateWindow**, passando -1 para todos os valores de margem.
+- Este programa construído em Assembly x86 obtém a imagem do dia do site Bing através do endpoint https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=pt-BR e define como papel de parede.
 
 ### Importante
 - Para construir e executar este programa utilize o build.bat localizado na raíz deste repositório:
 
 ```shell
-build.bat glass
+build.bat wallpaper
 ```
--- wallpaper/wallpaper.asm
+++ wallpaper/wallpaper.asm
@@ -14,122 +14,117 @@ INCLUDELIB \wininet.lib
 WinMain proto :DWORD,:DWORD,:DWORD,:DWORD
 
 .DATA                     			  
-_BytesToRead equ 10000 
-_wallPath  db "E:\Guilherme\Pictures\MEROMEI - Copia.jpg",0
+_BytesToRead equ 1024
+_fileName  db "\wallpaper.jpg",0
 _agent    db "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41",0
 _endpoint  db "https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=pt-BR",0
+_baseURL  db "https://www.bing.com",0
 _urlTagBegin db "<url>",0
 _urlTagEnd db "</url>",0
 
 .DATA?
 _InternetHandle DWORD ?
+_UrlHandle DWORD ?
 _FileHandle DWORD ?
-_Response db 10000 dup(?)
+_CurrentDirectory db 1024 dup(?)
+_wallpaperPath db 1024 dup(?)
+_Response db 1024 dup(?)
 _BytesRead DWORD ?
-_imageURL db 200 dup(?)
+_BytesWritten DWORD ?
+_imageURL db 300 dup(?)
+_imageURLComplete db 300 dup(?)
 _urlTagBeginAddress DWORD ?
 _urlTagEndAddress DWORD ?
 _urlLength DWORD ?
 
 .CODE
+INCLUDE string_macros.inc
+
 _start:
 
-;INCLUDE custom_macros.inc
+; OPEN CONNECTION **************************************************************
 
-_GetImageURL:
     ; Get Internet Handler
     invoke InternetOpen, addr _agent, INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, NULL
     test eax, eax
     jz _CloseConnection
     mov [_InternetHandle],eax
 
+
+; GET WALLPAPER URL ************************************************************
+
     ; Call API URL
     invoke InternetOpenUrl, _InternetHandle, addr _endpoint, NULL, NULL, NULL, NULL
     test eax, eax
     jz _CloseConnection
-    mov [_FileHandle],eax
+    mov [_UrlHandle],eax
 
     ; Get API Response
-    invoke InternetReadFile, _FileHandle, addr _Response, _BytesToRead, addr _BytesRead
+    invoke InternetReadFile, _UrlHandle, addr _Response, _BytesToRead, addr _BytesRead
     test eax, eax
     jz _CloseConnection
-    mov eax,[_BytesRead]
 
-    ; Extract URL from response string
-    cld ; clear direction flag
-    mov esi, offset _Response ; container string
-    mov edi, offset _urlTagBegin ; string to look for
-    mov ecx, _BytesRead ; length of container
+    ; Extract URL from response string    
+    str_InStr offset _Response, sizeof _Response, offset _urlTagBegin, sizeof _urlTagBegin, 0h, _urlTagBeginAddress
+    jne _CloseConnection
+    
+    str_InStr offset _Response, sizeof _Response, offset _urlTagEnd, sizeof _urlTagEnd, 0h, _urlTagEndAddress
+    jne _CloseConnection
 
-    search_urlTagBegin:
-        push ecx
-        push esi
-        push edi
+    add _urlTagBeginAddress, sizeof _urlTagBegin - 1
+    mov eax, _urlTagBeginAddress
+    mov ebx, _urlTagEndAddress
+    sub ebx, eax
+    mov _urlLength, ebx
 
-        mov ecx, sizeof _urlTagBegin - 1 ; discard null terminator
-        repe cmpsb
-        je found_urlTagBegin
+    str_Mid _urlTagBeginAddress, _urlLength, _imageURL
 
-        pop edi
-        pop esi
-        pop ecx
-        inc esi
-        loop search_urlTagBegin ; loop ecx times
-        jmp _CloseConnection
-        
-    found_urlTagBegin:
-        mov _urlTagBeginAddress, esi ; keep begin tag address
-
-        cld
-        mov esi, offset _Response 
-        mov edi, offset _urlTagEnd
-        mov ecx, _BytesRead
-
-    search_urlTagEnd:
-        push ecx
-        push esi
-        push edi
-
-        mov ecx, sizeof _urlTagEnd - 1
-        repe cmpsb
-        je found_urlTagEnd
-
-        pop edi
-        pop esi
-        pop ecx
-        inc esi
-        loop search_urlTagEnd
-        jmp _CloseConnection
-
-    found_urlTagEnd:
-        mov _urlTagEndAddress, esi ; keep end tag address
-        sub _urlTagEndAddress, sizeof _urlTagEnd - 1
-        mov eax, _urlTagBeginAddress
-        mov ebx, _urlTagEndAddress
-        sub ebx, eax
-        mov _urlLength, ebx
-
-        cld
-        mov esi, _urlTagBeginAddress
-        lea si, [esi] ;start position
-        lea di, _imageURL ;address of string to compose
-        mov ecx, _urlLength
-        rep movsb
-        mov al, 0
-        stosb
-        invoke MessageBox, 0, addr _imageURL, addr _imageURL, 0h
+    str_Concat offset _baseURL, offset _imageURL, offset _imageURLComplete
 
 
-_SetWallpaper:
-    invoke SystemParametersInfo, SPI_SETDESKWALLPAPER, 0, addr _wallPath, SPIF_UPDATEINIFILE
+; GET IMAGE STREAM AND WRITE ***************************************************
+
+    ; Set Filename to write    
+    invoke GetCurrentDirectory, _BytesToRead, addr _CurrentDirectory
+
+    str_Concat offset _CurrentDirectory, offset _fileName, offset _wallpaperPath
+
+    ; Call API URL
+    invoke InternetOpenUrl, _InternetHandle, addr _imageURLComplete, NULL, NULL, NULL, NULL
+    test eax, eax
+    jz _CloseConnection
+    mov [_UrlHandle],eax
+
+    ; Create / overwrite file for append
+    invoke CreateFile, addr _wallpaperPath, FILE_APPEND_DATA, FILE_SHARE_READ, NULL, CREATE_ALWAYS, 
+            FILE_ATTRIBUTE_NORMAL, NULL
+    test eax, eax
+    jz _CloseConnection
+    mov [_FileHandle],eax    
+
+    ; Get image stream
+    _readImageBytes:
+
+        invoke InternetReadFile, _UrlHandle, addr _Response, _BytesToRead, addr _BytesRead
+
+        invoke WriteFile, _FileHandle, addr _Response, _BytesRead, addr _BytesWritten, NULL
+
+        cmp _BytesRead, 0
+        jne _readImageBytes
+
+
+; SET IMAGE AS WALLPAPER *******************************************************
+
+    invoke SystemParametersInfo, SPI_SETDESKWALLPAPER, 0, addr _wallpaperPath, SPIF_UPDATEINIFILE
     jmp _CloseConnection
 
+
+; QUIT PROGRAM *****************************************************************
 
 _CloseConnection:
 
     invoke InternetCloseHandle, [_FileHandle]
     invoke InternetCloseHandle, [_InternetHandle]
-
 
 _Quit:
     invoke ExitProcess, 0 
