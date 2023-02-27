c765c7a35e84a1857f7158602028d1b79138a018
Guilherme Vieira Dutra <guilhermegvd@users.noreply.github.com>
2023-02-27 11:29:52 -0300

    minor fixes

--- wallpaper/wallpaper.asm
+++ wallpaper/wallpaper.asm
@@ -7,15 +7,15 @@ INCLUDE C:\masm32\include\user32.inc
 INCLUDE C:\masm32\include\kernel32.inc
 INCLUDE C:\masm32\include\wininet.inc
 
-INCLUDELIB \kernel32.lib
-INCLUDELIB \user32.lib    
-INCLUDELIB \wininet.lib    
+INCLUDELIB C:\masm32\lib\kernel32.lib
+INCLUDELIB C:\masm32\lib\user32.lib    
+INCLUDELIB C:\masm32\lib\wininet.lib    
 
 WinMain proto :DWORD,:DWORD,:DWORD,:DWORD
 
 .DATA                     			  
 _BytesToRead equ 1024
-_fileName  db "\wallpaper.jpg",0
+_fileName  db "wallpaper.jpg",0
 _agent    db "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41",0
 _endpoint  db "https://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=pt-BR",0
 _baseURL  db "https://www.bing.com",0
@@ -26,7 +26,7 @@ _urlTagEnd db "</url>",0
 _InternetHandle DWORD ?
 _UrlHandle DWORD ?
 _FileHandle DWORD ?
-_CurrentDirectory db 1024 dup(?)
+_TempDirectory db 1024 dup(?)
 _wallpaperPath db 1024 dup(?)
 _Response db 1024 dup(?)
 _BytesRead DWORD ?
@@ -84,19 +84,18 @@ _start:
 
 ; GET IMAGE STREAM AND WRITE ***************************************************
 
-    ; Set Filename to write    
-    invoke GetCurrentDirectory, _BytesToRead, addr _CurrentDirectory
-
-    str_Concat offset _CurrentDirectory, offset _fileName, offset _wallpaperPath
-
     ; Call API URL
     invoke InternetOpenUrl, _InternetHandle, addr _imageURLComplete, NULL, NULL, NULL, NULL
     test eax, eax
     jz _CloseConnection
     mov [_UrlHandle],eax
 
+    ; Set Filename to write    
+    invoke GetTempPath, _BytesToRead, addr _TempDirectory
+    str_Concat offset _TempDirectory, offset _fileName, offset _wallpaperPath
+
     ; Create / overwrite file for append
-    invoke CreateFile, addr _wallpaperPath, FILE_APPEND_DATA, FILE_SHARE_READ, NULL, CREATE_ALWAYS, 
+    invoke CreateFile, addr _wallpaperPath, GENERIC_WRITE, FILE_SHARE_READ, NULL, CREATE_ALWAYS, 
             FILE_ATTRIBUTE_NORMAL, NULL
     test eax, eax
     jz _CloseConnection
@@ -112,18 +111,20 @@ _start:
         cmp _BytesRead, 0
         jne _readImageBytes
 
+    ; Close file
+    invoke CloseHandle, [_FileHandle]
+
 
 ; SET IMAGE AS WALLPAPER *******************************************************
-
+    
     invoke SystemParametersInfo, SPI_SETDESKWALLPAPER, 0, addr _wallpaperPath, SPIF_UPDATEINIFILE
-    jmp _CloseConnection
 
 
 ; QUIT PROGRAM *****************************************************************
 
 _CloseConnection:
 
-    invoke InternetCloseHandle, [_FileHandle]
+    invoke InternetCloseHandle, [_UrlHandle]
     invoke InternetCloseHandle, [_InternetHandle]
 
 _Quit:
